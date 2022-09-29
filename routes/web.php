7a210a8e89d12f7bcbb9bb801d0f381a611e5f5b
Jhon Arcila Castano <jhon380172793@ucaldas.edu.co>
2022-09-28 20:44:46 -0400

    login and post and routes

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', [PageController::class, 'home'])->name('home');
-// Route::get('/blog', [PageController::class, 'blog'])->name('blog');
-// Route::get('/blog/{slug}', [PageController::class, 'post'])->name('post');
 
 Route::controller(PageController::class)->group(function () {
     Route::get('/', 'home')->name('home');
     Route::get('/blog', 'blog')->name('blog');
     Route::get('/blog/{post:slug}', 'post')->name('post');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+require __DIR__ . '/auth.php';
